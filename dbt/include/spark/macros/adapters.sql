badadb990137eee5e25257411f6a2ae5fcd7ca62
Drew Banin <drew@fishtownanalytics.com>
2019-12-04 15:10:45 -0500

    wip

--- dbt/include/spark/macros/adapters.sql
+++ dbt/include/spark/macros/adapters.sql
@@ -112,7 +112,8 @@
   {%- endcall %}
 {% endmacro %}
 
-{% macro spark__load_csv_rows(model, batch_size, agate_table) %}
+{% macro spark__load_csv_rows(model, batch_size) %}
+    {% set agate_table = model['agate_table'] %}
     {% set cols_sql = ", ".join(agate_table.column_names) %}
     {% set bindings = [] %}
 
@@ -126,9 +127,7 @@
         {% endfor %}
 
         {% set sql %}
-            insert into {{ this.render(False) }}
-            /* Expected column list: ({{ cols_sql }}) */
-            values
+            insert into {{ this.render(False) }} values
             {% for row in chunk -%}
                 ({%- for column in agate_table.column_names -%}
                     %s
@@ -148,3 +147,42 @@
     {# Return SQL so we can render it out into the compiled files #}
     {{ return(statements[0]) }}
 {% endmacro %}
+
+
+{% macro spark__reset_csv_table(model, full_refresh, old_relation) %}
+    {% if old_relation %}
+        {{ adapter.drop_relation(old_relation) }}
+    {% endif %}
+    {% set sql = create_csv_table(model) %}
+    {{ return(sql) }}
+{% endmacro %}
+
+
+{% materialization seed, adapter='spark' %}
+
+  {%- set identifier = model['alias'] -%}
+  {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
+  {%- set csv_table = model["agate_table"] -%}
+
+  {{ run_hooks(pre_hooks, inside_transaction=False) }}
+
+  -- `BEGIN` happens here:
+  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
+  -- build model
+  {% set create_table_sql = reset_csv_table(model, full_refresh_mode, old_relation) %}
+  {% set status = 'CREATE' %}
+  {% set num_rows = (csv_table.rows | length) %}
+  {% set sql = load_csv_rows(model) %}
+
+  {% call noop_statement('main', status ~ ' ' ~ num_rows) %}
+    {{ create_table_sql }};
+    -- dbt seed --
+    {{ sql }}
+  {% endcall %}
+
+  {{ run_hooks(post_hooks, inside_transaction=True) }}
+  -- `COMMIT` happens here
+  {{ adapter.commit() }}
+  {{ run_hooks(post_hooks, inside_transaction=False) }}
+{% endmaterialization %}
