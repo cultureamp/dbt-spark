eec386aca3258762a5cafc149496c680684b4de2
bhardwajsum <56814315+bhardwajsum@users.noreply.github.com>
2020-04-02 12:34:12 +1100

    Revert "Syncing the fishtown dbt spark to CA fork"

--- dbt/adapters/spark/relation.py
+++ dbt/adapters/spark/relation.py
@@ -1,24 +1,47 @@
-from dataclasses import dataclass
-
-from dbt.adapters.base.relation import BaseRelation, Policy
+from dbt.adapters.base.relation import BaseRelation
 
 
-@dataclass
-class SparkQuotePolicy(Policy):
-    database: bool = False
-    schema: bool = False
-    identifier: bool = False
-
-
-@dataclass
-class SparkIncludePolicy(Policy):
-    database: bool = False
-    schema: bool = True
-    identifier: bool = True
-
-
-@dataclass(frozen=True, eq=False, repr=False)
 class SparkRelation(BaseRelation):
-    quote_policy: SparkQuotePolicy = SparkQuotePolicy()
-    include_policy: SparkIncludePolicy = SparkIncludePolicy()
-    quote_character: str = '`'
+    DEFAULTS = {
+        'metadata': {
+            'type': 'SparkRelation'
+        },
+        'quote_character': '`',
+        'quote_policy': {
+            'database': False,
+            'schema': False,
+            'identifier': False,
+        },
+        'include_policy': {
+            'database': False,
+            'schema': True,
+            'identifier': True,
+        },
+        'dbt_created': False,
+
+    }
+
+    SCHEMA = {
+        'type': 'object',
+        'properties': {
+            'metadata': {
+                'type': 'object',
+                'properties': {
+                    'type': {
+                        'type': 'string',
+                        'const': 'SparkRelation',
+                    },
+                },
+            },
+            'type': {
+                'enum': BaseRelation.RelationTypes + [None]
+            },
+            'path': BaseRelation.PATH_SCHEMA,
+            'include_policy': BaseRelation.POLICY_SCHEMA,
+            'quote_policy': BaseRelation.POLICY_SCHEMA,
+            'quote_character': {'type': 'string'},
+            'dbt_created': {'type': 'boolean'},
+        },
+        'required': ['metadata', 'type', 'path', 'include_policy',
+                     'quote_policy', 'quote_character', 'dbt_created']
+    }
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from distutils.core import setup
 import os
 
 
@@ -9,20 +10,8 @@ with open(os.path.join(this_directory, 'README.md')) as f:
 
 
 package_name = "dbt-spark"
-package_version = "0.16.0a1"
+package_version = "0.14.3"
 description = """The SparkSQL plugin for dbt (data build tool)"""
-
-# evade bumpversion with this fun trick
-DBT_VERSION = (0, 16, 0)
-dbt_version = '.'.join(map(str, DBT_VERSION))
-# the package version should be the dbt version, with maybe some things on the
-# ends of it. (0.16.0 vs 0.16.0a1, 0.16.0.1, ...)
-if not package_version.startswith(dbt_version):
-    raise ValueError(
-        f'Invalid setup.py: package_version={package_version} must start with '
-        f'dbt_version={dbt_version} (from {DBT_VERSION})'
-    )
-
 
 setup(
     name=package_name,
@@ -45,7 +34,7 @@ setup(
         ]
     },
     install_requires=[
-        f'dbt-core=={dbt_version}',
+        'dbt-core=={}'.format(package_version),
         'PyHive[hive]>=0.6.0,<0.7.0',
         'thrift>=0.11.0,<0.12.0',
     ]
